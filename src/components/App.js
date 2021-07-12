aaf1180f0ee038dfaca93262bd5ac43c642f98df
Harold Torres Marino <haroldtm55@gmail.com>
2021-07-12 19:21:23 +1000

    Lab completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
@@ -15,6 +14,51 @@ class App extends React.Component {
     }
   }
 
+  handleChange = event => {
+    this.setState({
+      filters: {
+        type: event.target.value
+      }
+    })
+    
+  }
+  
+  fetchPets = () => {
+    if (this.state.filters.type === 'all') {
+      fetch('/api/pets').then(resp=>resp.json()).then(jsonPets=>{
+        this.setState({
+          pets:jsonPets
+        })
+      })
+    } else if (this.state.filters.type === 'cat') {
+      fetch('/api/pets?type=cat').then(resp=>resp.json()).then(jsonPets=>{
+        this.setState({
+          pets:jsonPets
+        })
+      })      
+    } else if (this.state.filters.type === 'dog') {
+      fetch('/api/pets?type=dog').then(resp=>resp.json()).then(jsonPets=>{
+        this.setState({
+          pets:jsonPets
+        })
+      })
+    } else {
+      fetch('/api/pets?type=micropig').then(resp=>resp.json()).then(jsonPets=>{
+        this.setState({
+          pets:jsonPets
+        })
+      })      
+    }      
+  }
+
+  handleAdoption = (petId) => {
+    this.state.pets.forEach(pet=> {
+      if (pet.id === petId) {
+        return pet.isAdopted = !pet.isAdopted
+      }
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +68,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.handleChange} onFindPetsClick={this.fetchPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.handleAdoption} pets={this.state.pets}/>
             </div>
           </div>
         </div>
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,25 +1,55 @@
 import React from 'react'
 
 class Pet extends React.Component {
+  constructor() {
+    super()
+    this.state={
+      className: "ui primary button",
+      buttonText: 'Adopt pet'
+    }
+  }
+  toggleAdoptPetButton = (event) => {
+    if (this.props.pet.isAdopted) {
+      return
+    }
+    this.props.onAdoptPet(this.props.pet.id)
+    this.setState({
+      className: "ui disabled button",
+      buttonText: 'Already adopted'
+    })
+  }
+
+  generateClassNameBasedOnAdoption = () => {
+    if (this.props.pet.isAdopted) {
+      return 'ui disabled button'
+    } else return 'ui primary button'
+  }
+
+  generateButtonTextBasedOnAdoption = () => {
+    if (this.props.pet.isAdopted) {
+      return 'Already adopted'
+    } else return 'Adopt pet'
+  }
+
   render() {
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+            {this.props.pet.gender==='male' ? '♂' : '♀'}
+            {this.props.pet.name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          {this.props.pet.isAdopted && <button className="ui disabled button">Already adopted</button>}
+          {!this.props.pet.isAdopted && <button onClick={(e)=>this.toggleAdoptPetButton(e)} className="ui primary button">Adopt pet</button>}
         </div>
       </div>
     )
